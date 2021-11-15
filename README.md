f929ffd3ea74a1894064544bcfe0db2394d4c1c7
rafalb8 <rafalb8@hotmail.com>
2021-11-15 15:34:15 +0100

    Update build script

--- README.md
+++ README.md
@@ -3,17 +3,19 @@ Build custom Linux iso with Docker
 
 Inspired by: [docker-to-linux](https://github.com/iximiuz/docker-to-linux)
 
-Possible images
-* alpine-arch   (Alpine rootfs with Arch Linux kernel)
-* alpine-dCore  (Alpine rootfs with dCore-focal64 kernel)
-* alpine-debian (Alpine rootfs with Debian kernel)
-* alpine-ubuntu (Alpine rootfs with ubuntu-focal kernel)
+All images are built with Alpine rootfs 
+
+Possible kernels
+* arch   (Arch Linux kernel)
+* dCore  (dCore-focal64 kernel)
+* debian (Debian kernel)
+* ubuntu (ubuntu-impish kernel)
 
 # Building
 
 Run
 ```bash
-build.sh image-version
+build.sh kernel
 ```
 
 # Try it out in qemu
--- build.sh
+++ build.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-getImages() {
-    cfgDirs="config out overlay"
-    dirs=$(ls -xd */ | sed -e 's/\///g')
-    images=""
-    for dir in $dirs; do
-        if [ "$cfgDirs" = "${cfgDirs/$dir/}" ]; then
-            images="$images $dir"
-        fi
-    done
-}
-
 usage() {
     cat <<EOF
-usage: $0 image
+usage: $0 [image]
 possible images : $images
 EOF
-
     exit 1
 }
 
-# Get possible images list
-getImages
+# --- MAIN ---
 
-# Prepare dir
-rm -rf out
-mkdir -p out
+# Find possible images
+for image in build/*/Dockerfile.*; do
+    images=$(echo -e "$images\n${image##*.}")
+done
 
-selected="$1"
-if [ $# = 0 ] || [ "$images" = "${images/$selected/}" ]; then
+# Remove duplicates
+images=$(echo "$images" | sort -u)
+
+# Select kernel
+KERNEL="$1"
+
+# Check selected image
+if [ $# = 0 ] || [ "$images" = "${images/$KERNEL/}" ]; then
     usage
 fi
+
+# Merge Dockerfile stages
+for stage in build/*; do
+    for file in $stage/{Dockerfile.$KERNEL,Dockerfile}; do
+        if buf=$(cat $file 2>/dev/null) ; then
+            DOCKERFILE=$(echo -e "$DOCKERFILE\n$buf")
+            break
+        fi
+    done
+done
+
+# echo "$DOCKERFILE"
 
 # Enable BuildKit
 export DOCKER_BUILDKIT=1
 
 # Build iso and save to ./out
-docker build -t lfs -o ./out -f $selected/Dockerfile .
+echo "$DOCKERFILE" | docker build -t lfs -o ./out . -f -
